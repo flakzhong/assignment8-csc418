3b71bc24945c406b1ed04e40aeaeba05b9cd89b7
flakzhong <xuhong.zhong@mail.utoronto.ca>
2018-11-22 18:32:41 -0500

    supersampling commented

--- main.cpp
+++ main.cpp
@@ -11,10 +11,13 @@
 #include <memory>
 #include <limits>
 #include <functional>
+#include <pthread.h>
 
 
 int main(int argc, char * argv[])
 {
+  const int num_threads = 8;
+
   Camera camera;
   std::vector< std::shared_ptr<Object> > objects;
   std::vector< std::shared_ptr<Light> > lights;
@@ -29,12 +32,12 @@ int main(int argc, char * argv[])
   int height = 360;
   std::vector<unsigned char> rgb_image(3*width*height);
 
-  float jitterMatrix[4 * 2] = {
-    -1.0/4.0,  3.0/4.0,
-     3.0/4.0,  1.0/3.0,
-    -3.0/4.0, -1.0/4.0,
-     1.0/4.0, -3.0/4.0,
-  };
+  // float jitterMatrix[4 * 2] = {
+  //   -1.0/4.0,  3.0/4.0,
+  //    3.0/4.0,  1.0/3.0,
+  //   -3.0/4.0, -1.0/4.0,
+  //    1.0/4.0, -3.0/4.0,
+  // };
 
   // For each pixel (i,j)
   for(unsigned i=0; i<height; ++i) 
@@ -42,23 +45,35 @@ int main(int argc, char * argv[])
     for(unsigned j=0; j<width; ++j)
     {
       Eigen::Vector3d rgb(0,0,0);
-      for (int sample = 0; sample < 4; sample++) {
-        Eigen::Vector3d temp(0, 0, 0);
-        // Compute viewing ray
-        Ray ray;
-        viewing_ray(camera,i + jitterMatrix[2*sample],j + jitterMatrix[2*sample+1],width,height,ray);
-        
-        // Shoot ray and collect color
-        raycolor(ray,1.0,objects,lights,0,temp);
-        rgb += temp;
-      }
-
-      rgb /= 4.0;
+      Ray ray;
+      viewing_ray(camera,i,j,width,height,ray);
+      
+      // Shoot ray and collect color
+      raycolor(ray,1.0,objects,lights,0,rgb);
       auto clamp = [](double s){ return std::max(std::min(s,1.0),0.0);};
       rgb_image[0+3*(j+width*i)] = 255.0*clamp(rgb(0));
       rgb_image[1+3*(j+width*i)] = 255.0*clamp(rgb(1));
       rgb_image[2+3*(j+width*i)] = 255.0*clamp(rgb(2));
 
+    //   Eigen::Vector3d rgb(0,0,0);
+    //   for (int sample = 0; sample < 4; sample++) {
+    //     Eigen::Vector3d temp(0, 0, 0);
+    //     // Compute viewing ray
+    //     Ray ray;
+    //     viewing_ray(camera,i + jitterMatrix[2*sample],j + jitterMatrix[2*sample+1],width,height,ray);
+        
+    //     // Shoot ray and collect color
+    //     raycolor(ray,1.0,objects,lights,0,temp);
+    //     rgb += temp;
+    //   }
+
+    //   rgb /= 4.0;
+    //   auto clamp = [](double s){ return std::max(std::min(s,1.0),0.0);};
+    //   rgb_image[0+3*(j+width*i)] = 255.0*clamp(rgb(0));
+    //   rgb_image[1+3*(j+width*i)] = 255.0*clamp(rgb(1));
+    //   rgb_image[2+3*(j+width*i)] = 255.0*clamp(rgb(2));
+
+    // }
     }
   }
 
